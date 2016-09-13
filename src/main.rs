1ed7220b87e11e6818adb2ac52a5deb63d3f2f1d
Trevor DiMartino <trevor.dimartino@colorado.edu>
2016-09-13 17:32:37 -0600

    It lives!  Works to model cladogenesis in about 2 secs or less.

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,43 @@
-
-use std::collections::HashMap;
-
 extern crate rand;
 extern crate time;
 extern crate csv;
+extern crate rustc_serialize;
+#[macro_use]
+extern crate clap;
 
 use rand::random;
+use rand::distributions::normal::StandardNormal;
 use csv::Writer;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, RustcEncodable)]
 pub struct Species {
-  id: i64,            // evolution order, birthdate
+  id: usize,          // evolution order, birthdate
   mass: f64,          // mass (g)
-  death: i64,         // iteration went (or will go) extinct
+  death: usize,       // iteration went (or will go) extinct
 }
 
-const N = 5000;  // num species at equil, w/o size bias
-
-fn speciate() {
-
-}
-
-fn doom_timer(mass: f64) -> i64 {
-  // Determines how many more rounds until a species goes extinct
-  // by drawing from a geometric distribution
-
-  let beta = 1.0/(N as f64);     // baseline extinction rate
-  let rho = 0.025;    // rate of extinction increase
-  let p = (10.0 as f64).powf(rho * mass.log10() + beta.log10());
-
-  let x: f64 = random();
-  (x.log10() / (1.0 - p).log10()).ceil() as i64
-}
 
 fn main() {
-  // The likely-to-be-toyed-with variables
-  let x_min = 1.8;
-  let x_0 = 40.0;
+  let args = clap::App::new("Cladogenesis")
+    .version("0.1")
+    .author("Trevor DiMartino")
+    .about("A model of random walk evolution, based on Clauset and Erwin 2008")
+    .args_from_usage("\
+      -x --min=[x_min]            'Minimum species mass, g (default 1.8)'
+      -i --initial=[x_0]          'Initial species mass, g (default 40.0)'
+      -n --nspecies=[n]           'Estimate of number of species at equilibrium (default 5000)'
+      -a --writeall=[write_all]   'Write out birth order, mass, and death time of all species (default true)'")
+    .get_matches();
+
+  let x_min = value_t!(args.value_of("min"), f64).unwrap_or(1.8);
+  let x_0 = value_t!(args.value_of("initial"), f64).unwrap_or(40.0);
+  let n = value_t!(args.value_of("nspecies"), usize).unwrap_or(5000);
+  let write_all = value_t!(args.value_of("writeall"), bool).unwrap_or(true);
 
   // Determine how many steps to run
   let nu = 1.6;       // mean species lifetime (My)
   let tau = 60.0;     // total simulation time (My)
-  let t_max = ((tau / nu) * (N as f64)).round();
+  let t_max: usize = ((tau / nu) * (n as f64)).ceil() as usize;
 
   // Cope's Rule parameters
   let c1 = 0.33;      // log-lambda intercept
@@ -52,31 +48,84 @@ fn main() {
   let sigma = 0.63;   // variance
   let alpha = 0.30;   // power-law tail
 
-  let mut extant: Vec<f64> = 
-    Vec::with_capacity((N as f64 * 1.5).ceil() as usize);
-  let mut all_species: Vec<Species> =
-    Vec::with_capacity(N * 10);
+  // Extinctino parameters
+  let beta = 1.0/(n as f64);    // baseline extinction rate
+  let rho = 0.025;              // rate of extinction increase
+
+  let doom_timer = |mass: f64| {
+    // Determines how many more rounds until a species goes extinct
+    // by drawing from a geometric distribution
+    let p = (10.0 as f64).powf(rho * mass.log10() + beta.log10());
+
+    let x: f64 = random();
+    (x.log10() / (1.0 - p).log10()).ceil() as usize
+  };
+
+  let mut extant: Vec<(f64, usize)> = Vec::with_capacity((n as f64 * 1.5).ceil() as usize);
+  let doom = doom_timer(x_0);
+  extant.push((x_0, doom));
+
+  let mut all_species: Vec<Species> = Vec::with_capacity(0);
+  if write_all {
+    all_species = Vec::with_capacity(t_max + 1);
+    all_species.push(Species{id: 0, mass: x_0, death: doom});
+  }
+
 
   // Start timing
   let start = time::precise_time_ns();
 
+  for step in 1..t_max {
+    let ancestor: usize = (random::<f64>() * extant.len() as f64).floor() as usize;
 
+    let (mass_a, _) = extant[ancestor];
+    let l1 = mass_a / x_min;
 
-  let mut deaths = Vec::with_capacity(10000);
-  for ii in 0..10000 {
-    deaths.push(doom_timer(x_0));
+    // Apply Cope's rule, strengthened for smaller species
+    let mu: f64;
+    if mass_a.log10() < c2 {
+      mu = (-c1 / c2) * mass_a.log10() + c1 + delta;
+    } else {
+      mu = delta;
+    }
+
+    // Draw Monte Carlo factor
+    let mut tt: f64 = 0.0;
+    while tt < 1.0 / l1 {
+      let StandardNormal(r) = random();
+      tt = (r * sigma + mu).exp() * 
+        ((random::<f64>() * (1.0 - 1.0 / l1) + 1.0 / l1).powf(alpha)) /
+        (random::<f64>().powf(alpha));
+    }
+
+    let mass_d = mass_a * tt;
+    let doom = doom_timer(mass_d) + step;
+    extant.push((mass_d, doom));
+    if write_all { all_species.push(Species{id: step, mass: mass_d, death: doom}); }
+
+    // Retain species whose doom timer is later than when we are now
+    // ... that is to say, kill off the now-dead ones
+    extant.retain(|&(_, d)| d > step);
   }
-
-  let path = format!("deaths{}.csv", x_0);
-  let mut writer = Writer::from_file(path).unwrap();
-  for d in deaths.into_iter() {
-    writer.encode(d).ok().expect("CSV writer error");
-  }
-
-
 
   // End timing
   let end = time::precise_time_ns();
 
-  println!("Ran in {} seconds.", (end - start) / (1000000000));
+  // Print out our final set of extant species
+  let path = format!("extant_{}_{}_{}.csv", x_min, x_0, n);
+  let mut writer = Writer::from_file(path).unwrap();
+  for s in extant.into_iter() {
+    writer.encode(s).ok().expect("CSV writer error");
+  }
+
+  if write_all {
+    // All species from the HashMap
+    let path = format!("all_{}_{}_{}.csv", x_min, x_0, n);
+    let mut writer = Writer::from_file(path).unwrap();
+    for s in all_species.into_iter() {
+      writer.encode(s).ok().expect("CSV writer error");
+    }
+  }
+
+  println!("Ran in {} seconds.", (end - start) / 1000000000);
 }
